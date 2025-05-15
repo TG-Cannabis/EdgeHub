cdb46bae6bca3aecfb674f68a92a621f12381552
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-05-15 12:45:44 -0500

    feat: initializing project

---
+++ src/main/java/com/tgcannabis/edgehub/EdgeHubApplication.java
@@ -0,0 +1,13 @@
+package com.tgcannabis.edgehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgcannabis/edgehub/EdgeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgcannabis.edgehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
