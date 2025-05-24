5af9038bd0223446dbaffb6cb55688f3d31965f8
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2025-05-23 19:05:26 -0500

    fix: loading config from env

--- src/main/java/com/tgcannabis/edgehub/config/InfluxConfig.java
+++ src/main/java/com/tgcannabis/edgehub/config/InfluxConfig.java
@@ -2,25 +2,32 @@ package com.tgcannabis.edgehub.config;
 
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InfluxConfig {
 
+    @Value("${influx.url}")
+    private String influxUrl;
+
+    @Value("${influx.token}")
+    private String influxToken;
+
+    @Value("${influx.org}")
+    private String influxOrg;
+
+    @Value("${influx.bucket}")
+    private String influxBucket;
+
     @Bean
     public InfluxDBClient influxDBClient() {
-        String url = "http://localhost:8086";
-        char[] token = "dc4d53ba57bb6bae9dcab955fb62204e794208fa15cb824877a99768085d6c7c".toCharArray();
-        String org = "tgcannabis";
-        String bucket = "sensor_data";
-
         return InfluxDBClientFactory.create(
-                url,
-                token,
-                org,
-                bucket
+                influxUrl,
+                influxToken.toCharArray(),
+                influxOrg,
+                influxBucket
         );
     }
-
 }
